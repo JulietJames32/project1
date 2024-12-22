2f78f849b4f617f84c542a8ea51dcaa97f5258d1
JulietJames32 <JulietJamesThottappallil@gmail.com>
2024-12-22 09:53:47 +0400

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -130,8 +130,8 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
  
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='jamesjuliet72@gmail.com'
+EMAIL_HOST_USER=
 EMAIL_HOST_PASSWORD=
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-DEFAULT_FROM_EMAIL='jamesjuliet72@gmail.com'
+DEFAULT_FROM_EMAIL=
